13502c0b7f382b4b00d2e193922fd2d8ec0c0fb4
Quick <31828688+Quick104@users.noreply.github.com>
2026-06-25 14:40:17 -0400

    feat(plugin): declare image resolver capability

--- main.go
+++ main.go
@@ -60,6 +60,7 @@ type runtimeServer struct {
 
 type metadataServer struct {
 	pluginv1.UnimplementedMetadataProviderServer
+	pluginv1.UnimplementedImageResolverServer
 	runtime *runtimeServer
 }
 
@@ -287,10 +288,12 @@ func main() {
 		provider: provider.NewProvider(),
 	}
 
+	ms := &metadataServer{runtime: rs}
 	runtime.Serve(runtime.ServeConfig{
 		Servers: runtime.CapabilityServers{
 			Runtime:          rs,
-			MetadataProvider: &metadataServer{runtime: rs},
+			MetadataProvider: ms,
+			ImageResolver:    ms,
 		},
 	})
 }
